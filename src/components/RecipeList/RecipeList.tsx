104c0bbaf4ef2b4d46330c91fcc0f4f27caf3d83
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-05-22 05:49:44 +0000

    link added

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -5,10 +5,10 @@ import './RecipeList.css'
 
 import { Link } from 'react-router-dom'
 
-interface IRecipeList {
-    data:any
-}
-export default function RecipeList({data}:IRecipeList){
+// interface IRecipeList {
+//     data:any
+// }
+export default function RecipeList({data}:any){
     return (
         <div className='Recipe-List'>
     {
@@ -17,7 +17,7 @@ export default function RecipeList({data}:IRecipeList){
                 <h3>{recipe.title}</h3>
                 <p>{recipe.cookingTime} to make.</p>
                 <div>{recipe.method.substring(0,100)}...</div>
-                <Link to={`/recipe/${recipe.id}`}>Cook this</Link>
+                <Link  to={`/recipe/${recipe.id}`}>Cook this</Link>
            </div>
           )) 
     }
--- src/pages/recipi/Recipe.tsx
+++ src/pages/recipi/Recipe.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import{ useState } from 'react'
 import './Recipe.css';
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/082 useFetch';
 
 export default function Recipe() {
+   let {id="id"}:any  = useParams();
+  const url = "http://localhost:3000/recipes/" + id; 
+  const  { data, isPending, error } = useFetch(url); 
+  console.log(data);
+
   return (
     <div>
-       <h3>Recipes</h3>
+       {isPending && <p>Loading</p> }
+       {error && <p>{error}</p> }
+       { data && data?.map((response?:any) =>(
+        <div>{ response.id}</div>
+       ))}
     </div>
   )
 }
